04d4468e0b17fd655a5b96edf8125ee8ae86f13d
Jeremiah Wuenschel <jeremiah.wuenschel@gmail.com>
2014-02-12 22:35:12 -0800

    Added missing (empty) default attributes for gtalk

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,6 @@ default['hubot']['group'] = 'users'
 default['hubot']['home'] = '/home/hubot'
 default['hipchat']['jabber_id'] = ''
 default['hipchat']['password'] = ''
+
+default[:gtalk][:username] = ''
+default[:gtalk][:password] = ''
