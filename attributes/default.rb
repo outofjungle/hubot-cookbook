abd248cb3852478329bdf4b47b98d43a38bad194
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2014-02-27 17:42:23 -0800

    refactored most code out of default recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,8 @@ default[:hubot][:user] = 'hubot'
 default[:hubot][:group] = 'users'
 default[:hubot][:home] = '/home/hubot'
 
-default[:hipchat][:jabber_id] = ''
-default[:hipchat][:password] = ''
+default[:hipchat][:jabber_id] = 'hipchat_jabber_id'
+default[:hipchat][:password] = 'hipchat_password'
 
-default[:gtalk][:username] = ''
-default[:gtalk][:password] = ''
+default[:gtalk][:username] = 'gtalk_username'
+default[:gtalk][:password] = 'gtalk_password'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,3 @@
 #
 
 bootstrap
-
-hubot_hipchat 'secretsauce' do
-  env ({
-    'HUBOT_HIPCHAT_JID' => node[:hipchat][:jabber_id],
-    'HUBOT_HIPCHAT_PASSWORD' => node[:hipchat][:password]
-  })
-end
-
-hubot_gtalk 'secretsauce' do
-  env ({
-      'HUBOT_GTALK_USERNAME' => node[:gtalk][:username],
-      'HUBOT_GTALK_PASSWORD' => node[:gtalk][:password]
-  })
-end
